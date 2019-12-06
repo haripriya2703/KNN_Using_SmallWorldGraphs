2ea068ef88c7625f2c27f3e13a72fe143ff6beb5
haripriya2703 <haripriyaponnekanti@gmail.com>
2019-12-06 00:13:23 -0500

    corrections

--- User_Interaction.py
+++ User_Interaction.py
@@ -3,19 +3,28 @@ from KNN_Classifier import knn_classifier
 from Data_Point import DataPoint
 
 
-print "Input the features of your data point"
-petal_length = input("Petal length?")
-petal_width = input("Petal Width?")
-sepal_length = input("Sepal Length?")
-sepal_width = input("Sepal Width?")
-data_point = DataPoint(petal_length, petal_width, sepal_length, sepal_width)
-m = 2
-k = 7
-choice = input("1-Insert and Classify, 2-Classify")
-if choice == 1:
-    knn_graph = knn_data_insertion([], data_point, m, k)
-    print "Label of data point: ", knn_classifier(knn_graph, data_point, m, k)
-elif choice == 2:
-    print "Label of data point: ", knn_classifier([], data_point, m, k)
-else:
-    print "Incorrect choice"
+knn_graph = []
+while True:
+    print "Input the features of your data point"
+    petal_length = input("Petal length?")
+    petal_width = input("Petal Width?")
+    sepal_length = input("Sepal Length?")
+    sepal_width = input("Sepal Width?")
+    data_point = DataPoint(petal_length, petal_width, sepal_length, sepal_width)
+    m = 2
+    k = 7
+    while True:
+        choice = input("1-Insert and Classify, 2-Classify")
+        if choice == 1:
+            knn_graph = knn_data_insertion(knn_graph, data_point, m, k)
+            print "Label of data point: ", knn_classifier(knn_graph, data_point, m, k)
+            break
+        elif choice == 2:
+            print "Label of data point: ", knn_classifier(knn_graph, data_point, m, k)
+            break
+        else:
+            print "Incorrect choice"
+
+    more_data = input("Do you want to input more data? Y/N")
+    if more_data == 'N' or more_data == 'n':
+        break
